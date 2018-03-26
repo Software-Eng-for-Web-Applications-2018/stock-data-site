1835465fc549915ca813a76f24152f029e2ef3d1
Kevin <kpielacki@gmail.com>
2018-03-26 19:18:00 -0400

    Secret key include and more descriptive

--- example_config.py
+++ example_config.py
@@ -1,5 +1,19 @@
-# Example config.py file containing sensitive information
+'''example_config.py
+
+Example config.py file containing sensitive information
+'''
+
 
 # SQL Alchemy SQL URI
 # http://flask-sqlalchemy.pocoo.org/2.3/config/
 MYSQL_DB_URI = '<flavor>://<user>:<password>@<host>:<port>/<db>'
+
+
+# Secrety key for crypto exchange
+# Generate this key yourself:
+#     python3
+#     import os
+#     os.urandom(24)
+#
+# For more info visit http://flask.pocoo.org/docs/0.12/quickstart/
+SECRET_KEY = 'Paste the generate value here!'

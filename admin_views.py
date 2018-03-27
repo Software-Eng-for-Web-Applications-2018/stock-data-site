208c68207d3fa48ae1154cd1ce894fcd97485ecc
Kevin <kpielacki@gmail.com>
2018-03-26 20:41:28 -0400

    new mvc views

--- admin_views.py
+++ admin_views.py
@@ -1,9 +1,10 @@
 from app import db
-from models import User
+from models import (StockPriceDay, StockPriceMinute, User)
 from views.home_view import HomeView
 from views.login_view import LoginView
 from views.logout_view import LogoutView
 from views.user_view import UserView
+from views.stock_price_view import StockPriceView
 
 
 def add_admin_views(admin, app):
@@ -11,17 +12,26 @@ def add_admin_views(admin, app):
     admin.add_view(HomeView(name='Home', endpoint='home'))
 
     # User dash view handling
-    #admin.add_view(DashView(name='User Portal', endpoint='userdash',
-    #               app=app))
-    #admin.add_view(BusinessDashView(name='Business Portal',
-    #                                endpoint='businessdash', app=app))
+    #admin.add_view(RealtimeDashView(
+    #    name='Realtime Portal',
+    #    endpoint='realtimeportal',
+    #    app=app
+    #))
+
+    # Model view handling
+    admin.add_view(StockPriceView(
+        StockPriceDay,
+        db.session,
+        name='Daily Stock Prices')
+    )
+    admin.add_view(StockPriceView(
+        StockPriceMinute,
+        db.session,
+        name='Minute Stock Prices')
+    )
 
     # Admin portal views
     admin.add_view(UserView(User, db.session, name='Users'))
-    #admin.add_view(HazardSummaryView(
-    #    HazardSummary, db.session, name='Hazard Summary'))
-    #admin.add_view(HazardLocationView(
-    #    HazardLocation, db.session, name='Hazard Locations'))
 
     # Login and Logout views
     admin.add_view(LoginView(name='Login', endpoint='login'))

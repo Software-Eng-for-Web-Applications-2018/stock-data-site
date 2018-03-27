66a17325aca504a27ea2fa853dcd87d4304e13af
Kevin <kpielacki@gmail.com>
2018-03-26 20:40:36 -0400

    MVC for stock price data

---
+++ views/stock_price_view.py
@@ -0,0 +1,16 @@
+from flask_admin.contrib import sqla
+from secure_views import SecureModelView
+
+
+class StockPriceView(SecureModelView):
+
+    can_create = False
+    can_edit = False
+    can_delete = False
+
+    column_list = ('dateid', 'sym', 'volume', 'close', 'high', '_open', 'low')
+    column_searchable_list = ('dateid', 'sym')
+    column_filters = column_list
+    column_labels = {'dateid': 'Date', 'sym': 'Symbol'}
+    column_default_sort = ('dateid', 'sym')
+    column_sortable_list = column_list

0daf0e3356d3a4d2b2dbc5b645253bd831d25015
Kevin Pielacki <kpielacki@gmail.com>
2018-04-26 00:21:54 +0000

    set timeout for faster response in case of error

--- utils.py
+++ utils.py
@@ -38,9 +38,19 @@ class PredictionRequest(object):
 
     resource = 'inference'
 
+    timeout = 0.4
+
     def get_pred(self, data, freq, ml_type, sym):
         url = '/'.join((self.root_url, self.resource, freq, ml_type, sym))
-        r = requests.post(url, data=json.dumps(data), headers=self.headers)
+        try:
+            r = requests.post(
+                url,
+                data=json.dumps(data),
+                headers=self.headers,
+                timeout=self.timeout
+            )
+        except:
+            return {}
         if r.status_code != 200:
             return {}
 
